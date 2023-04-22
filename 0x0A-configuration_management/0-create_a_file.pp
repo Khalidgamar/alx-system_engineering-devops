704f9d775e628d068be230dc45073ee7495aad22
Khalidgamar <khgamar97@gmail.com>
2023-04-22 12:39:41 -0700

    tasks

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+reate a file in /tmp
+
+file { 'school':
+ensure  => 'present',
+path    => '/tmp/school',
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
+content => 'I love Puppet'
+}
