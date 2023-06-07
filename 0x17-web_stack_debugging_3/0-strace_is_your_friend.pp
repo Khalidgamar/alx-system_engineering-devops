1f116817db88e1efbe99abb31df88f9474bb3575
Khalid_Gamar <khgamar97@gmail.com>
2023-06-07 00:12:16 +0000

    API A

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,11 @@
+# Fixing Apache returning a 500 error
+
+exec { 'fix error':
+  provider => 'shell',
+  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
+}# Fixing Apache returning a 500 error
+
+exec { 'fix error':
+  provider => 'shell',
+  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
+}
